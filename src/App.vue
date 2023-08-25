80fa6b6cb5e9e7a463461818e1f61517d72099c0
Lionell Briones <llenoil@gmail.com>
2023-08-25 11:15:01 +0800

    test vue-tag

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { event } from 'vue-gtag'
+export default {
+  methods: {
+    buttonClick() {
+      event('button_click', {
+        event_category: 'engagement',
+        event_label: 'click',
+        value: 1
+      })
+    }
+  },
+  mounted() {
+    console.log('App mounted!')
+  }
+}
+</script>
 
 <template>
   <div>Home</div>
+  <div>1. Track button click</div>
+  <button @click="buttonClick">Click</button>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import VueGtag from 'vue-gtag'
 import App from './App.vue'
 
 const app = createApp(App)
+
+app.use(VueGtag, {
+  config: { id: 'G-WPPHHHRZ1T' },
+})
 
 app.mount('#app')
